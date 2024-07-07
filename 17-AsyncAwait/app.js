e429447c860086828416662c1d30a48c269cecf4
ProjectRavel <rafaelcodeid@gmail.com>
2024-07-08 02:06:51 +0700

    Menambahkan Materi ES6

---
+++ 17-AsyncAwait/app.js
@@ -0,0 +1,40 @@
+// const coba = new Promise((resolve) => {
+//   setTimeout(() => {
+//     resolve('selesai')
+//   },2000);
+// })
+
+// coba.then(()=>{
+//   console.log(coba)
+// })
+
+function cobaPromise() {
+  return new Promise((resolve, reject) => {
+    const waktu = 3000
+    if(waktu < 5000){
+      setTimeout(() => {
+        resolve('selesai')
+      }, waktu);
+    }else{
+      reject('kelamaan')
+    }
+  })
+}
+
+// const coba = cobaPromise()
+// coba.then(() => {
+//   console.log(coba)
+// })
+
+async function cobaAsync() {
+ try{
+  const coba = await cobaPromise()
+  console.log(coba)
+ }catch(err){
+  console.log(err)
+ }
+}
+
+cobaAsync()
+
+// gabisa gw gabisa.. 
